02dc9d351cdd58a8ecfe26969e3d7d1b8634523b
Mateusz Korkosz <korkosz.mateusz@gmail.com>
2021-12-09 22:12:58 +0100

    working renamed base template

--- src/BitBagSyliusMultiCartPlugin.php
+++ src/BitBagSyliusMultiCartPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace BitBag\SyliusMultiCartPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class BitBagSyliusMultiCartPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
