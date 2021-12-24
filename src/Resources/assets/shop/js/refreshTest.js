1d9f1ef8e5e0b0e03db39fd94342c3f22b87382c
Mateusz Korkosz <korkosz.mateusz@gmail.com>
2021-12-24 07:10:24 +0100

    Update - "js wip"

--- src/Resources/assets/shop/js/refreshTest.js
+++ src/Resources/assets/shop/js/refreshTest.js
@@ -2,4 +2,37 @@
 //     alert("I am an alert box!");
 // }
 
-console.log('test')
+console.log('TESTO')
+// function changeCart(){
+//     $('#sylius-cart-total').html('TEST');
+//     $.ajax({
+//         type: "GET",
+//         url: '/en_US/_partial/cart/summary',
+//         data: {'template': '@SyliusShop/Cart/_widget.html.twig'},
+//         success:function(response){
+//         // $('#test_click').html(response);
+//         $('#sylius-cart-total').content.replaceWith('test');
+//         },
+//     });
+// }
+
+// global.changeCart = function() {
+//     console.log('testy')
+// }
+
+function changeCart() {
+    console.log('testy')
+}
+
+// $(document).ready(function() {
+//     console.log('super test')
+//     $('#korek-test').click(function() {
+//         // alert("costam dziala");
+//     });
+// })
+
+var refreshButton = document.getElementById('korek-test');
+
+refreshButton.addEventListener('click', function () {
+    alert('proper event listener')
+})
