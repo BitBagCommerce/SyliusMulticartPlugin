cf583d8b7f15209d10958b0b7e5d2ccdc31a5481
milwoz <milwoz95@gmail.com>
2022-06-03 15:13:09 +0200

    Switched to use trait

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -14,15 +14,5 @@ use Sylius\Component\Core\Model\Customer as BaseCustomer;
 
 class Customer extends BaseCustomer implements CustomerInterface
 {
-    protected ?int $activeCart = 1;
-
-    public function getActiveCart(): ?int
-    {
-        return $this->activeCart;
-    }
-
-    public function setActiveCart(?int $activeCart): void
-    {
-        $this->activeCart = $activeCart;
-    }
+    use CustomerTrait;
 }
