9cc211b797f5e5afd9210c409e5490fb53a7f011
Gabriela Lubkowska <dsw45017@student.dsw.edu.pl>
2023-03-26 13:38:33 +0200

    Add Sylius 1.12 support

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 return [
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
-    FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
-    Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle::class => ['all' => true],
 ];
