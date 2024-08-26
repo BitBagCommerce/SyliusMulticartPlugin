b363ba5b44a08b8366b60f3dde571645c8c909c9
Tomasz Kaliński <tomkalon@gmail.com>
2024-08-26 11:21:19 +0200

    OP-451-refactored - PHPUnit fixes

--- tests/PHPUnit/Integration/OrderRepositoryTest.php
+++ tests/PHPUnit/Integration/OrderRepositoryTest.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusMultiCartPlugin\PHPUnit\Integration;
 
-use Tests\BitBag\SyliusMultiCartPlugin\Application\src\Repository\OrderRepositoryInterface;
+use BitBag\SyliusMultiCartPlugin\Repository\OrderRepositoryInterface;
 
 class OrderRepositoryTest extends IntegrationTestCase
 {
